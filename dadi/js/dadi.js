761f0b5cf2b3273bbd55352b4bab34560bda1321
Antonius131 <antomendetta@gmail.com>
2022-06-17 16:49:27 +0200

    !!DICE create two const to generate 1 to 6 numbers

---
+++ dadi/js/dadi.js
@@ -0,0 +1,8 @@
+// devo generare un numero randomico da 1 a 6 per il giocatore
+// devo generare un numero randomico da 1 a 6 per il computer
+// devo stabilire un vincitore in base al punteggio più alto
+
+const playerDice = Math.floor(Math.random() * (6) + 1);
+console.log(playerDice);
+const computerDice = Math.floor(Math.random() * (6) + 1);
+console.log(computerDice)
