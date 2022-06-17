bb23547869dede507c28be8ec288596638724b16
Antonius131 <antomendetta@gmail.com>
2022-06-17 14:56:43 +0200

    creating prompt request + array mailList

---
+++ mail/js/mail.js
@@ -0,0 +1,15 @@
+// devo chiedere all'utente la sua email (prompt())
+// devo verificare che sia presente nella lista
+// stampo un messaggio sull'esito del controllo
+
+const mailRequest = prompt('Inserisci la tua email');
+const mailList = [
+   'pinco@gmail.com',
+   'pallo@gmail.com',
+   'cip@gmail.com',
+   'ciop@gmail.com',
+   'qui@gmail.com',
+   'quo@gmail.com',
+   'qua@gmail.com',
+];
+
